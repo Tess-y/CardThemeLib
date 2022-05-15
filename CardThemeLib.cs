41e6d35048a55d6ea816de5677a4d60d82e8e747
Tess <tess@tessyac.com>
2022-05-15 09:56:38 -0400

    Add project files.

---
+++ CardThemeLib.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CardThemeLib
+{
+    // Declares our Mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our Mod Is associated with
+    [BepInProcess("Rounds.exe")]
+    public class CardThemeLib : BaseUnityPlugin
+    {
+        private const string ModId = "root.cardtheme.lib";
+        private const string ModName = "Card Theme Extention Library";
+        public const string Version = "1.0.0";
+        internal Dictionary<string, CardThemeColor> themes = new Dictionary<string, CardThemeColor>();
+        public IReadOnlyDictionary<string, CardThemeColor> Themes { get { return themes; } }
+
+        public static CardThemeLib instance { get; private set; }
+
+        void Awake()
+        {
+            instance = this;
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+            CardThemeColor[] cardThemeColors = CardChoice.instance.cardThemes.ToArray();
+            string[] cardThemeNames = new string[] { "DestructiveRed", "FirepowerYellow", "DefensiveBlue", "TechWhite", "EvilPurple", "PoisonGreen", "NatureBrown", "ColdBlue", "MagicPink" };
+            for (int i = 0; i < cardThemeColors.Length; i++)
+            {
+                CreateOrGetType(cardThemeNames[i], cardThemeColors[i]);
+            }
+
+        }
+
+        public CardThemeColor.CardThemeColorType CreateOrGetType(string name, CardThemeColor themeColor = null)
+        {
+            if (themes.ContainsKey(name)) return themes[name].themeType;
+            else
+            {
+                CardThemeColor.CardThemeColorType themeType = (CardThemeColor.CardThemeColorType)themes.Count;
+                themeColor.themeType = themeType;
+                themes.Add(name, themeColor);
+                CardChoice.instance.cardThemes = themes.Values.ToArray();
+                return themeType;
+            }
+        }
+
+    }
+}
